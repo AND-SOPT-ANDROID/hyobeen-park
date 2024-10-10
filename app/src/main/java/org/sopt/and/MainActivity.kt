d82e4ed5a83513a866071d2e5d620af1c45ea5cd
Hyobeen-Park <amanda0109@naver.com>
2024-10-10 18:23:05 +0900

    [FEAT/#1] 로그인 -> 회원가입 이동 구현

--- app/src/main/java/org/sopt/and/MainActivity.kt
+++ app/src/main/java/org/sopt/and/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import org.sopt.and.ui.theme.ANDANDROIDTheme
+import org.sopt.and.core.designsystem.theme.ANDANDROIDTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/org/sopt/and/feature/signin/SignInActivity.kt
+++ app/src/main/java/org/sopt/and/feature/signin/SignInActivity.kt
@@ -1,44 +1,37 @@
 package org.sopt.and.feature.signin
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import org.sopt.and.R
-import org.sopt.and.core.designsystem.component.SocialLoginButton
-import org.sopt.and.core.designsystem.component.textfield.EmailTextField
-import org.sopt.and.core.designsystem.component.textfield.PasswordTextField
+import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.and.core.designsystem.component.topappbar.BackButtonTopAppBar
 import org.sopt.and.core.designsystem.theme.ANDANDROIDTheme
 import org.sopt.and.feature.signup.SignUpActivity
 
+const val ID_KEY = "id"
+const val PASSWORD_KEY = "password"
+
+@AndroidEntryPoint
 class SignInActivity : ComponentActivity() {
+    private var id: String = ""
+    private var password: String = ""
+
+    val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                id = result.data?.getStringExtra(ID_KEY) ?: ""
+                password = result.data?.getStringExtra(PASSWORD_KEY) ?: ""
+            }
+
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -51,162 +44,19 @@ class SignInActivity : ComponentActivity() {
                         )
                     }
                 ) { innerPadding ->
-                    SignInScreen(
+                    SignInRoute(
+                        id,
+                        password,
                         navigateToSignUp = {
-                            Intent(this, SignUpActivity::class.java).apply {
-                                startActivity(this)
-                            }
+                            val intent = Intent(this, SignUpActivity::class.java)
+                            resultLauncher.launch(intent)
                         },
+                        navigateToMyPage = {},
                         modifier = Modifier
-                            .padding(paddingValues = innerPadding)
+                            .padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun SignInScreen(
-    navigateToSignUp: () -> Unit,
-    modifier: Modifier = Modifier,
-) {
-    val email = remember { mutableStateOf("") }
-    val password = remember { mutableStateOf("") }
-    val isPasswordVisible = remember { mutableStateOf(false) }
-
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-            .background(Color.Black),
-    ) {
-        EmailTextField(
-            email = email,
-            placeholder = stringResource(R.string.sign_in_email),
-            modifier = Modifier
-                .padding(top = 50.dp)
-                .padding(horizontal = 20.dp),
-        )
-        PasswordTextField(
-            password = password,
-            placeholder = stringResource(R.string.sign_in_password),
-            isVisible = isPasswordVisible,
-            modifier = Modifier
-                .padding(top = 5.dp)
-                .padding(horizontal = 20.dp),
-        )
-
-        Button(
-            onClick = {},
-            shape = RoundedCornerShape(20.dp),
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 20.dp)
-                .padding(horizontal = 20.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Blue,
-            )
-        ) {
-            Text(
-                text = stringResource(R.string.sign_in),
-                color = Color.White,
-            )
-        }
-
-        Row(
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 15.dp),
-        ) {
-            Text(
-                text = stringResource(R.string.sign_in_find_id),
-                fontSize = 12.sp,
-                color = Color.Gray,
-                modifier = Modifier
-                    .padding(horizontal = 8.dp),
-            )
-            Box(
-                modifier = Modifier
-                    .size(width = 1.dp, height = 10.dp)
-                    .background(Color.Gray)
-            )
-            Text(
-                text = stringResource(R.string.sign_in_change_password),
-                fontSize = 12.sp,
-                color = Color.Gray,
-                modifier = Modifier
-                    .padding(horizontal = 8.dp),
-            )
-            Box(
-                modifier = Modifier
-                    .size(width = 1.dp, height = 10.dp)
-                    .background(Color.Gray)
-            )
-            Text(
-                text = stringResource(R.string.sign_up_main_title),
-                fontSize = 12.sp,
-                color = Color.Gray,
-                modifier = Modifier
-                    .padding(horizontal = 8.dp)
-                    .clickable {
-                        navigateToSignUp()
-                    },
-            )
-        }
-
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                .padding(top = 50.dp),
-        ) {
-            Box(
-                modifier = Modifier
-                    .height(1.dp)
-                    .weight(1f)
-                    .background(Color.DarkGray),
-            )
-            Text(
-                text = stringResource(R.string.sign_up_social_description),
-                fontSize = 12.sp,
-                color = Color.Gray,
-                modifier = Modifier
-                    .padding(horizontal = 10.dp),
-            )
-            Box(
-                modifier = Modifier
-                    .height(1.dp)
-                    .weight(1f)
-                    .background(Color.DarkGray),
-            )
-        }
-
-        SocialLoginButton(
-            modifier = Modifier
-                .padding(top = 20.dp)
-        )
-
-        Text(
-            text = stringResource(R.string.sign_up_information),
-            fontSize = 12.sp,
-            color = Color.Gray,
-            modifier = Modifier
-                .padding(
-                    top = 40.dp,
-                    start = 20.dp,
-                    end = 20.dp
-                ),
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun SignInPreview() {
-    ANDANDROIDTheme {
-        SignInScreen(
-            navigateToSignUp = { },
-        )
     }
 }
