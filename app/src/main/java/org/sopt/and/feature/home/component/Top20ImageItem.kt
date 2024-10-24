5bc743670b3e6e532d303e37625c95d0d2753645
Hyobeen-Park <amanda0109@naver.com>
2024-10-24 23:25:10 +0900

    [FEAT/#4] top20 아이템 구현

---
+++ app/src/main/java/org/sopt/and/feature/home/component/Top20ImageItem.kt
@@ -0,0 +1,51 @@
+package org.sopt.and.feature.home.component
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
+
+@Composable
+fun Top20ImageItem(
+    image: String,
+    rank: Int,
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        contentAlignment = Alignment.BottomStart,
+        modifier = modifier
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(image)
+                .crossfade(true)
+                .build(),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .padding(bottom = 30.dp)
+                .size(height = 240.dp, width = 200.dp)
+                .clip(RoundedCornerShape(10.dp))
+        )
+        Text(
+            text = rank.toString(),
+            fontSize = 50.sp,
+            color = White,
+            modifier = Modifier
+                .padding(start = 10.dp)
+        )
+    }
+}
