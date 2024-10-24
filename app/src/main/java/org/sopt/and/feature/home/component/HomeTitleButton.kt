5b88863c3a490df0559009b10e85422c1c8039ec
Hyobeen-Park <amanda0109@naver.com>
2024-10-24 23:35:29 +0900

    [FEAT/#4] 홈 타이틀 컴포넌트 구현

---
+++ app/src/main/java/org/sopt/and/feature/home/component/HomeTitleButton.kt
@@ -0,0 +1,49 @@
+package org.sopt.and.feature.home.component
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.R
+
+@Composable
+fun HomeTitleButton(
+    title: String,
+    onClick: () -> Unit = {},
+    modifier: Modifier = Modifier,
+) {
+    TextButton(
+        onClick = onClick,
+        content = {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = modifier,
+            ) {
+                Text(
+                    text = title,
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .weight(1f)
+                )
+                Icon(
+                    painter = painterResource(R.drawable.ic_arrow_right_24),
+                    contentDescription = title,
+                    tint = Color.Gray,
+                    modifier = Modifier
+                        .size(30.dp)
+                )
+            }
+        }
+    )
+}
