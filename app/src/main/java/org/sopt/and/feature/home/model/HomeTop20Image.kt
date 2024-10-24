e79e8be2569e98d0bf91003a1906ad38b269b2d6
Hyobeen-Park <amanda0109@naver.com>
2024-10-25 01:29:05 +0900

    [FEAT/#4] 더미데이터 추가

---
+++ app/src/main/java/org/sopt/and/feature/home/model/HomeTop20Image.kt
@@ -0,0 +1,14 @@
+package org.sopt.and.feature.home.model
+
+fun getHomeTop20Image(): List<String> = listOf(
+    "https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202309/1694062313324743958.webp",
+    "https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202409/1726468463104944317.webp",
+    "https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202311/1698908212143109040.webp",
+    "https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202410/1728017508965610221.webp",
+    "https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202311/1700742608486397915.webp",
+    "https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202311/1699601586197251349.webp",
+    "https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202307/1690523370531767356.webp",
+    "https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202307/1688707846915136240.webp",
+    "https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202405/1715069627941981919.webp",
+    "https://image.wavve.com/v1/thumbnails/240_360_20_80/meta/image/202410/1728613201561457273.webp",
+)
--- app/src/main/java/org/sopt/and/feature/home/model/Image.kt
+++
@@ -1,6 +0,0 @@
-package org.sopt.and.feature.home.model
-
-data class HomeSliderImage(
-    val title: String,
-    val image: String,
-)
