9fd828cd7084e69b987333d4dfeaeeec0df686c1
Hyobeen-Park <amanda0109@naver.com>
2024-10-24 22:19:00 +0900

    [ADD/#4] coil 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,4 +69,7 @@ dependencies {
 
     implementation(libs.androidx.compose.navigation)
     implementation(libs.kotlinx.serialization.json)
+
+    implementation(libs.coil.compose)
+    implementation(libs.coil.network)
 }
