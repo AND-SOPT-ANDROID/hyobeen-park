5f34a46cec99d13ff27995604751a24ceebf61d2
Hyobeen-Park <amanda0109@naver.com>
2024-10-25 00:04:20 +0900

    [FEAT/#4] 홈 탑바 구현

---
+++ app/src/main/java/org/sopt/and/feature/home/component/HomeTopBar.kt
@@ -0,0 +1,40 @@
+package org.sopt.and.feature.home.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import org.sopt.and.R
+
+@Composable
+fun HomeTopBar() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 10.dp, horizontal = 20.dp),
+    ) {
+        Image(
+            painter = painterResource(R.drawable.img_main_logo),
+            contentDescription = stringResource(R.string.wavve),
+            modifier = Modifier
+                .width(100.dp),
+        )
+        Icon(
+            painter = painterResource(R.drawable.ic_live_tv_24),
+            contentDescription = stringResource(R.string.wavve),
+            tint = Color.White,
+        )
+    }
+}
